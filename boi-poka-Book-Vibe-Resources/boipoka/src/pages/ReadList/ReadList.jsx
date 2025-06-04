78d4548a834c4e7676b42b48216529219dbad92f
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-06-04 19:06:12 +0600

    added sort by pages and sort by ratings from the readlist

--- boi-poka-Book-Vibe-Resources/boipoka/src/pages/ReadList/ReadList.jsx
+++ boi-poka-Book-Vibe-Resources/boipoka/src/pages/ReadList/ReadList.jsx
@@ -11,6 +11,7 @@ const ReadList = () => {
     // normally eivabe korena
 
     const [readList, setReadList]=useState([]);
+    const [sort,setSort]=useState("");
 
     const data=useLoaderData();
     console.log(data);
@@ -23,28 +24,46 @@ const ReadList = () => {
         setReadList(myReadList);
     },[])
 
+    const handleSort=(type)=>{
+        setSort(type);
+        if(type==='Pages'){
+            const sortedByPage=[...readList].sort((a,b)=>a.totalPages-b.totalPages);
+            setReadList(sortedByPage);
+        }
+        if(type==='Ratings'){
+            const sortedByRating=[...readList].sort((a,b)=>a.rating-b.rating);
+            setReadList(sortedByRating);
+        }
+    }
 
     return (
         <div>
-            <Tabs className='my-14'>
-    <TabList>
-      <Tab>Read Books ({readList.length})</Tab>
-      <Tab>My Wish List</Tab>
-    </TabList>
+                <details className="dropdown">
+                <summary className="btn m-1">Sort By : {sort?sort:""}</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a onClick={()=>handleSort("Pages")}>Pages</a></li>
+                    <li><a onClick={()=>handleSort("Ratings")}>Ratings</a></li>
+                </ul>
+                </details>
+        <Tabs className='my-14'>
+        <TabList>
+        <Tab>Read Books ({readList.length})</Tab>
+        <Tab>My Wish List</Tab>
+        </TabList>
 
-    <TabPanel>
-      <div className='my-14'>
-        {
-        readList.map(b=><SingleBookDetail key={b.bookId} b={b}></SingleBookDetail>)
-      }
-      </div>
-    </TabPanel>
-    <TabPanel>
-      <h2>My wishlist books are here</h2>
-    </TabPanel>
-  </Tabs>
+        <TabPanel>
+        <div className='my-14'>
+            {
+            readList.map(b=><SingleBookDetail key={b.bookId} b={b}></SingleBookDetail>)
+        }
         </div>
-    );
+        </TabPanel>
+        <TabPanel>
+        <h2>My wishlist books are here</h2>
+        </TabPanel>
+    </Tabs>
+            </div>
+        );
 };
 
 export default ReadList;
